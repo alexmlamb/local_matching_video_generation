23d9e6da937b69eb74954be8cbc2b173585d5678
Alexander Lamb <lambalex@leto27.iro.umontreal.ca>
2017-09-09 16:01:13 -0400

    derp!

--- main.py
+++ main.py
@@ -62,7 +62,43 @@ d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0003)
 g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
 c_optimizer = torch.optim.Adam(C.parameters(), lr=0.0003)
 
+for epoch in range(200):
+    for i, (images, _) in enumerate(data_loader):
 
+        batch_size = images.size(0)
+
+        images = to_var(images.view(batch_size, -1))
+
+        real_labels = to_var(torch.ones(batch_size))
+        fake_labels = to_var(torch.zeros(batch_size))
+        boundary_labels = to_var(0.5 * torch.ones(batch_size))
+
+        outputs = D(images)
+        d_loss_real = ((outputs - real_labels)**2).mean()
+        real_score = outputs
+
+        z = to_var(torch.randn(batch_size, 64))
+        fake_images = G(z)
+        outputs = D(fake_images)
+        d_loss_fake = ((outputs - fake_labels)**2).mean()
+        fake_score = outputs
+
+        d_loss = d_loss_real + d_loss_fake
+
+        D.zero_grad()
+        d_loss.backward()
+        d_optimizer.step()
+
+        #z = to_var(torch.randn(batch_size, 64))
+        #fake_images = G(z)
+        outputs_critic = C(to_var(fake_images.data))
+        c_loss = ((outputs_critic - to_var(fake_score.data))**2).mean()
+
+        C.zero_grad()
+        c_loss.backward()
+        c_optimizer.step()
+
+        print outputs_critic.mean()
 
 
 
