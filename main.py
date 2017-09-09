12b8f6246f6321c1b6e0a98b4c69ce7050539612
Alexander Lamb <lambalex@leto27.iro.umontreal.ca>
2017-09-09 15:06:12 -0400

    adding torch code

--- main.py
+++ main.py
@@ -1,9 +1,69 @@
 import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.autograd import Variable, grad
+
+'''
+Initially just implement LSGAN on MNIST.  
+
+Then implement a critic.  
+'''
+
+def to_var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+def denorm(x):
+    out = (x+1)/2
+    return out.clamp(0,1)
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))])
+
+mnist = datasets.MNIST(root='./data/', train=True, download=True, transform=transform)
+
+data_loader = torch.utils.data.DataLoader(dataset=mnist, batch_size=100, shuffle=True)
 
 #Discriminator
+D = nn.Sequential(
+    nn.Linear(784, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 1))
+
 #Critic
-#Actor
-#PostActor
+C = nn.Sequential(
+    nn.Linear(784, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 1))
+
+# Generator 
+G = nn.Sequential(
+    nn.Linear(64, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 784),
+    nn.Tanh())
+
+if torch.cuda.is_available():
+    D.cuda()
+    C.cuda()
+    G.cuda()
+
+d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0003)
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
+c_optimizer = torch.optim.Adam(C.parameters(), lr=0.0003)
+
+
+
 
 
 
