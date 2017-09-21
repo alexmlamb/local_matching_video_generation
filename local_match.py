7d3edf0ceb34711d7076a7be53b92775425105e6
Alexander Lamb <lambalex@kepler3.iro.umontreal.ca>
2017-09-21 02:06:12 -0400

    updates

--- local_match.py
+++ local_match.py
@@ -26,6 +26,8 @@ def denorm(x):
     out = (x+1)/2
     return out.clamp(0,1)
 
+batch_size = 100
+
 real_labels = to_var(torch.ones(batch_size))
 fake_labels = to_var(torch.zeros(batch_size))
 boundary_labels = to_var(0.5 * torch.ones(batch_size))
@@ -34,13 +36,15 @@ transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean
 
 mnist = datasets.MNIST(root='./data/', train=True, download=True, transform=transform)
 
-data_loader = torch.utils.data.DataLoader(dataset=mnist, batch_size=100, shuffle=True)
 
-nz = 64
+data_loader = torch.utils.data.DataLoader(dataset=mnist, batch_size=batch_size, shuffle=True)
+
+
+nz = 128
 ns = 2
 
 #(zL,xL) and (zR,xR)
-D_bot = nn.Sequential(
+d_bot = nn.Sequential(
     nn.Linear(784/ns + nz, 256),
     nn.LeakyReLU(0.2),
     nn.Linear(256, 256),
@@ -48,7 +52,7 @@ D_bot = nn.Sequential(
     nn.Linear(256, 1))
 
 #(z,zL,zR)
-D_top = nn.Sequential(
+d_top = nn.Sequential(
     nn.Linear(nz*ns + nz, 256),
     nn.LeakyReLU(0.2),
     nn.Linear(256, 256),
@@ -99,14 +103,14 @@ gen_top = nn.Sequential(
     nn.Linear(256, ns*nz),
     nn.Tanh())
 
-models = [D_top, D_bot, inf_bot, gen_bot, inf_top, gen_top]
+models = [d_top, d_bot, inf_bot, gen_bot, inf_top, gen_top]
 
 if torch.cuda.is_available():
     for model in models: 
         model.cuda()
 
-d_top_optimizer = torch.optim.Adam(D_top.parameters(), lr=0.0003)
-d_bot_optimizer = torch.optim.Adam(D_bot.parameters(), lr=0.0003)
+d_top_optimizer = torch.optim.Adam(d_top.parameters(), lr=0.0003)
+d_bot_optimizer = torch.optim.Adam(d_bot.parameters(), lr=0.0003)
 inf_bot_optimizer = torch.optim.Adam(inf_bot.parameters(), lr=0.0003)
 gen_bot_optimizer = torch.optim.Adam(gen_bot.parameters(), lr=0.0003)
 inf_top_optimizer = torch.optim.Adam(inf_top.parameters(), lr=0.0003)
@@ -116,63 +120,99 @@ gen_top_optimizer = torch.optim.Adam(gen_top.parameters(), lr=0.0003)
 for epoch in range(200):
     for i, (images, _) in enumerate(data_loader):
 
-        batch_size = images.size(0)
+        #batch_size = images.size(0)
 
         #====
         #Inference Procedure
+
+        #gen_bot, then 
+
         #====
 
         images = to_var(images.view(batch_size, -1))
 
+        z_bot_lst = []
+        for seg in range(0,ns):
+            xs = images[:,seg*(784/ns):(seg+1)*(784/ns)]
+            zs = inf_bot(xs)
 
-        outputs_left = D(images[:,:784/2])
-        d_loss_real_left = ((outputs_left - real_labels)**2).mean()
-        real_score = outputs_left
+            d_out_bot = d_bot(torch.cat((zs,xs),1))
+            
+            d_loss_bot = ((d_out_bot - real_labels)**2).mean()
+            g_loss_bot = ((d_out_bot - boundary_labels)**2).mean()
 
-        z = to_var(torch.randn(batch_size, 64))
-        fake_images = G(z)
-        outputs_left = D(fake_images)
-        outputs_right = 
-        d_loss_fake = ((outputs - fake_labels)**2).mean()
-        fake_score = outputs
+            d_bot.zero_grad()
+            d_loss_bot.backward(retain_graph=True)
+            d_bot_optimizer.step()
 
-        #Feed inference to discriminators
+            inf_bot.zero_grad()
+            g_loss_bot.backward()
+            inf_bot_optimizer.step()
 
-        d_loss_left = d_loss_real_left + d_loss_fake_left
-        d_loss_right = d_loss_real_right + d_loss_fake_right
+            z_bot_lst.append(zs)
 
-        D_left.zero_grad()
-        D_right.zero_grad()
-        d_loss.backward()
-        d_optimizer.step()
+        z_bot = torch.cat(z_bot_lst, 1)
+
+        z_bot = Variable(z_bot.data)
+
+        z_top = inf_top(z_bot)
+
+        d_out_top = d_top(torch.cat((z_top, z_bot), 1))
+
+        d_loss_top = ((d_out_top - real_labels)**2).mean()
+        g_loss_top = ((d_out_top - boundary_labels)**2).mean()
+
+        d_top.zero_grad()
+        d_loss_top.backward(retain_graph=True)
+        d_top_optimizer.step()
+
+        inf_top.zero_grad()
+        g_loss_top.backward()
+        inf_top_optimizer.step()
 
 
         print "epoch", epoch
         #============GENERATION PROCESS========================$
 
-        z_top = to_var(torch.randn(batch_size, 64))
+        z_top = to_var(torch.randn(batch_size, nz))
 
         z_bot = gen_top(z_top)
 
-        gen_x_lst = []
+        d_out_top = d_top(torch.cat((z_top, z_bot),1))
 
+        d_top.zero_grad()
+
+        d_loss_top = ((d_out_top - fake_labels)**2).mean()
+        d_loss_top.backward(retain_graph=True)
+        d_top_optimizer.step()
+
+        g_loss_bot = ((d_out_top - boundary_labels)**2).mean()
+        gen_bot.zero_grad()
+        d_bot.zero_grad()
+        g_loss_bot.backward()
+        gen_top_optimizer.step()
+
+        z_bot = Variable(z_bot.data)
+
+        gen_x_lst = []
         for seg in range(0,ns):
             seg_z = z_bot[:,seg*nz:(seg+1)*nz]
             seg_x = gen_bot(seg_z)
             gen_x_lst.append(seg_x)
-            d_out_bot = d_bot(join_bot_d(seg_x, seg_z))
+            d_out_bot = d_bot(torch.cat((seg_z, seg_x),1))
             d_loss_bot = ((d_out_bot - fake_labels)**2).mean()
             
             d_bot.zero_grad()
             d_loss_bot.backward(retain_graph=True)
-            d_bot_optimizer().step()
+            d_bot_optimizer.step()
 
             g_loss_bot = ((d_out_bot - boundary_labels)**2).mean()
             gen_bot.zero_grad()
             d_bot.zero_grad()
             g_loss_bot.backward()
-            g_bot_optimizer.step()
+            gen_bot_optimizer.step()
 
+        print d_out_bot
 
 
     fake_images = fake_images.view(fake_images.size(0), 1, 28, 28)
